65cd52abf1717bc98cc824a8fef6a7231c4abbba
Багдан Абдуев <bvabduev@ya.ru>
2023-08-14 14:49:51 +0300

    осталось последнее задание

--- anfisa_for_friends/anfisa_for_friends/settings.py
+++ anfisa_for_friends/anfisa_for_friends/settings.py
@@ -20,10 +20,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'django.shortcuts',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -31,6 +34,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+] 
 
 ROOT_URLCONF = 'anfisa_for_friends.urls'
 
@@ -79,7 +86,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    iice_cream_list = IceCream.objects.values('id', 'title', 'description').filter(is_published=True, is_on_main=True).order_by('title').values('id', 'title', 'description')[:3]
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template, context)
+
